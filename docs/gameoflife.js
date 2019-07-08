bdfe9cd788adea3e03df2f0d6531f35ea8a5e2f8
brisberg <brandon.risberg@gmail.com>
2019-07-08 07:41:04 -0700

    First attempt at initializing data from a file. Issue with module imports for es2015

--- docs/gameoflife.js
+++ docs/gameoflife.js
@@ -1,12 +1,13 @@
-var WORLD_SIZE = 100;
-var world = [];
-var adjacent = [];
-var tableElement;
+import { data } from './map-data';
+let worldSize = 100;
+const world = [];
+const adjacent = [];
+let tableElement;
 function setup() {
-    for (var i = 0; i < WORLD_SIZE; i++) {
+    for (let i = 0; i < worldSize; i++) {
         world[i] = [];
         adjacent[i] = [];
-        for (var j = 0; j < WORLD_SIZE; j++) {
+        for (let j = 0; j < worldSize; j++) {
             world[i][j] = 0;
             adjacent[i][j] = 0;
         }
@@ -14,30 +15,22 @@ function setup() {
     tableElement = document.body.firstElementChild;
     // load the images?
     // Initialize the map
-    // down right glider
-    world[10][10] = 1;
-    world[11][11] = 1;
-    world[11][12] = 1;
-    world[10][12] = 1;
-    world[9][12] = 1;
-    // right MWSS
-    world[40][10] = 1;
-    world[40][11] = 1;
-    world[40][12] = 1;
-    world[40][13] = 1;
-    world[40][14] = 1;
-    world[39][14] = 1;
-    world[38][14] = 1;
-    world[37][13] = 1;
-    world[36][11] = 1;
-    world[37][9] = 1;
-    world[39][9] = 1;
-    setInterval(function () { return update(); }, 200);
+    if (data.size) {
+        worldSize = data.size;
+    }
+    if (data.world) {
+        for (let i = 0; i < worldSize; i++) {
+            for (let j = 0; j < worldSize; j++) {
+                world[i][j] = data.world[i][j];
+            }
+        }
+    }
+    setInterval(() => update(), 200);
 }
 function update() {
     // 1 cell border to simplify calculation
-    for (var i = 1; i < WORLD_SIZE - 1; i++) {
-        for (var j = 1; j < WORLD_SIZE - 1; j++) {
+    for (let i = 1; i < worldSize - 1; i++) {
+        for (let j = 1; j < worldSize - 1; j++) {
             adjacent[i][j] =
                 world[i - 1][j - 1] +
                     world[i - 1][j] +
@@ -49,8 +42,8 @@ function update() {
                     world[i + 1][j + 1];
         }
     }
-    for (var i = 1; i < WORLD_SIZE - 1; i++) {
-        for (var j = 1; j < WORLD_SIZE - 1; j++) {
+    for (let i = 1; i < worldSize - 1; i++) {
+        for (let j = 1; j < worldSize - 1; j++) {
             if (world[i][j]) {
                 // active cell
                 if (adjacent[i][j] === 2 || adjacent[i][j] === 3) {
@@ -71,9 +64,9 @@ function update() {
     render();
 }
 function render() {
-    for (var i = 1; i < WORLD_SIZE - 1; i++) {
-        for (var j = 1; j < WORLD_SIZE - 1; j++) {
-            var cell = tableElement.firstElementChild.children[i].children[j];
+    for (let i = 1; i < worldSize - 1; i++) {
+        for (let j = 1; j < worldSize - 1; j++) {
+            const cell = tableElement.firstElementChild.children[i].children[j];
             if (world[i][j]) {
                 cell.classList.add('cell');
             }
@@ -83,4 +76,4 @@ function render() {
         }
     }
 }
-window.onload = function () { return setup(); };
+window.onload = () => setup();
